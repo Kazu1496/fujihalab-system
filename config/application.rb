57e4c4eb7db7c90b96a155b61e50401978d277b7
2357gi <2357.gi@gmail.com>
2019-02-08 15:44:17 +0900

    [wip] api/v1/existenceへのIP制限 修正 gemの追記はしたけどbundle updateはしてない。 研究室のIP調べて追記する必要あり。

--- config/application.rb
+++ config/application.rb
@@ -15,16 +15,12 @@ module InAnyoneLab
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-  end
-end
-
-module Sample
-  class Application < Rails::Application
     config.middleware.use Rack::Access,{
         "api/v1/existence" => ["127.0.0.1", LAB_IP_ADDRESS]
     }
   end
 end
+
 
 private
 LAB_IP_ADDRESS = '000.000.000.000'
