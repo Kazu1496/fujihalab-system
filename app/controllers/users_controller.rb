650c1abb46699ecef52652a0eac5e97f0cec3d4d
Kazumasa Ishikura <kazumasa1496@gmail.com>
2019-03-13 01:52:21 +0900

    Add: pixela機能実装/erbからhamlに拡張子変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,34 +7,8 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
-  # GET /users/1
-  # GET /users/1.json
-  def show
-  end
+  def show; end
 
-  # GET /users/new
-  def new
-    @user = User.new
-  end
-
-  # POST /users
-  # POST /users.json
-  def create
-    @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
   def update
     respond_to do |format|
       if @user.update(user_params)
@@ -56,6 +30,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :status, :total)
+      params.require(:user).permit(:name, :image)
     end
 end
---
+++ app/uploaders/image_uploader.rb
@@ -0,0 +1,17 @@
+class ImageUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+
+  storage :file
+
+  version :retina do
+    process resize_to_fit: [640, 640]
+  end
+
+  version :schema do
+    process resize_to_fill: [800, 800, "Center"]
+  end
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+end
