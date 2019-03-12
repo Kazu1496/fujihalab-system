ee9e417e3f51bec2c20e04306dd565dbd82d8567
2357gi <2357.gi@gmail.com>
2019-03-12 23:07:53 +0900

    :bug: 入退出履歴表示修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,20 @@
 module ApplicationHelper
   def datetime_encorder(target_datetime)
-    if (target_datetime = 0)
-      ""
+    if target_datetime.present?
+      target_datetime.strftime("%H:%M")
     else
-      target_datetime.strftime("%H時M分")
+      ""
     end
+  end
+  def stay_time(enter_at, exit_at)
+    if exit_at.present? && enter_at.present?
+      sec = (exit_at.to_time - enter_at.to_time).to_i
+      hour = sec / 3600
+      min = format("%0*d", 2, (sec / 60 - hour * 60))       #TODO: リファクタリングしろ
+      staytime = (format("%0*d", 2, hour).to_s + ':' + min)
+    else
+      staytime = '在室中'
+    end
+    staytime
   end
 end
