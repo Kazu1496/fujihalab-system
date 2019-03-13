a2e354f71bd4a029f77c9e1ea283aff852b9ad35
Kazumasa Ishikura <kazumasa1496@gmail.com>
2019-03-14 03:14:34 +0900

    Change: pixelaAPI周りを共通化/出退席時刻修正機能の実装

---
+++ app/controllers/api/v1/existences_controller.rb
@@ -0,0 +1,99 @@
+require 'net/https'
+require 'open-uri'
+require 'securerandom'
+
+class Api::V1::ExistencesController < ApplicationController
+  protect_from_forgery :except => [:post]
+
+  def post
+    # post test
+    # ```shell
+    # curl -X POST -H "Content-Type: application/json" -d '{"name": "Buri", "password": "secure" "status": true}' http://0.0.0.0:3000/api/v1/existence
+    # ```
+    now_time = Time.now()
+    name = params[:name].downcase
+    password = params[:password]
+    status = params[:status]
+    user = User.find_by(name: name)
+
+    if user.blank?
+      user = User.new(
+        name: name,
+        password: password,
+        pixela_token: SecureRandom.hex(32)
+      )
+
+      # CreateUser
+      user_uri = URI.parse("https://pixe.la/v1/users/")
+      user_http = Net::HTTP.new(user_uri.host, user_uri.port)
+
+      user_http.use_ssl = true
+      user_http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+      user_post_params = {
+        token: user.pixela_token,
+        username: user.name,
+        agreeTermsOfService: "yes",
+        notMinor: "yes"
+      }
+
+      user_req = Net::HTTP::Post.new(user_uri)
+      user_req.body = user_post_params.to_json
+
+      user_res = user_http.request(user_req)
+      user_result = JSON.parse(user_res.body)
+
+      # CreateGraph
+      graph_uri = URI.parse("https://pixe.la/v1/users/#{user.name}/graphs")
+      graph_http = Net::HTTP.new(graph_uri.host, graph_uri.port)
+
+      graph_http.use_ssl = true
+      graph_http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+      graph_post_params = {
+        id: "access-graph",
+        name: "#{user.name}-graph",
+        unit: "hour",
+        type: "int",
+        color: "shibafu"
+      }
+
+      graph_req = Net::HTTP::Post.new(graph_uri)
+      graph_req.body = graph_post_params.to_json
+      graph_req["X-USER-TOKEN"] = user.pixela_token
+
+      graph_res = graph_http.request(graph_req)
+      graph_result = JSON.parse(graph_res.body)
+
+      if user_result["isSuccess"] && graph_result["isSuccess"]
+        user.save
+        render json: {status: 200, message: "User create successfully!"}
+      else
+        render json: {status: 400, message: "User create faild..."}
+      end
+    end
+
+
+    if status
+      existence = user.existences.order(:updated_at).create(user_id: user.id)
+      user.update!(status: true)
+      existence.update!(enter_time: now_time)
+    else
+      existences = Existence.where(
+        user_id: user.id,
+        enter_time: @existence.enter_time.in_time_zone.all_day
+      )
+
+      #delete_pixel
+      delete_pixel(user, now_time.strftime("%Y%m%d"))
+
+      if create_pixel(user, now_time, existences) # CreatePixel
+        user.update!(status: false)
+        existence.update!(exit_time: now_time, stay_time: diff)
+        render json: {status: 200, message: "Pixel create successfully!"}
+      else
+        render json: {status: 400, message: "Pixel create faild..."}
+      end
+    end
+  end
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,74 @@
 class ApplicationController < ActionController::Base
+  private
+    def create_pixel(user, date, existences)
+      quantity = stay_time(existences)
+
+      pixel_uri = URI.parse("https://pixe.la/v1/users/#{user.name}/graphs/access-graph")
+      pixel_http = Net::HTTP.new(pixel_uri.host, pixel_uri.port)
+
+      pixel_http.use_ssl = true
+      pixel_http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+      pixel_post_params = {
+        date: date.strftime("%Y%m%d"),
+        quantity: quantity.to_s
+      }
+
+      pixel_req = Net::HTTP::Post.new(pixel_uri)
+      pixel_req.body = pixel_post_params.to_json
+      pixel_req["X-USER-TOKEN"] = user.pixela_token
+
+      pixel_res = pixel_http.request(pixel_req)
+      pixel_result = JSON.parse(pixel_res.body)
+
+      pixel_result["isSuccess"]
+    end
+
+    def delete_pixel(user, date)
+      delete_pixel_uri = URI.parse("https://pixe.la/v1/users/#{user.name}/graphs/access-graph/#{date}")
+      delete_pixel_http = Net::HTTP.new(delete_pixel_uri.host, delete_pixel_uri.port)
+
+      delete_pixel_http.use_ssl = true
+      delete_pixel_http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+      delete_pixel_req = Net::HTTP::Delete.new(delete_pixel_uri)
+      delete_pixel_req["X-USER-TOKEN"] = user.pixela_token
+
+      delete_pixel_res = delete_pixel_http.request(delete_pixel_req)
+      delete_pixel_result = JSON.parse(delete_pixel_res.body)
+    end
+
+    def stay_time(existences)
+      staytime = 0
+      if existences.present?
+        existences.each do |existence|
+          sec = (existence.exit_time.to_time - existence.enter_time.to_time).to_i
+          staytime += sec / 3600
+        end
+      end
+      staytime
+    end
+
+    def total_time(user, existences, date)
+      existences.each do |existence|
+        diff = if date.present?
+                 (date - Time.parse(existence.enter_time.to_s)) / 60
+               else
+                 (existence.exit_time - Time.parse(existence.enter_time.to_s)) / 60
+               end
+        total_time = diff + user.total
+      end
+      total_time
+    end
+
+    def update_time(current_time, updated_time)
+      date = current_time.strftime("%Y%m%d")
+      datetime = updated_time.strftime("%H%M")
+      updated_date = date + datetime
+      updated_date.to_time
+    end
 end
+
 # 200 Success
 def response_success(class_name, action_name)
   render status: 200, json: { status: 200, message: "Success #{class_name.capitalize} #{action_name.capitalize}" }
