45b8891e17f869ebb0e5ee560939e193d57dc7db
Kazumasa Ishikura <kazumasa1496@gmail.com>
2019-03-16 02:30:27 +0900

    Fix: 在室情報変更機能周りのバグ修正

--- app/models/existence.rb
+++ app/models/existence.rb
@@ -1,7 +1,7 @@
 class Existence < ApplicationRecord
   belongs_to :user
 
-  scope :order_by_enter_at, -> { order(enter_time: :desc) }
+  scope :order_by_enter_at, -> { order(created_at: :desc) }
 
   def previous
     Existence.where("user_id = ? and id < ?", self.user_id, self.id).order("id DESC").first
