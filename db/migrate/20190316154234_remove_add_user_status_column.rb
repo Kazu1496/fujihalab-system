6bbe7f547593fc12d6d1af28dcf256f30fb487c4
Kazumasa Ishikura <kazumasa1496@gmail.com>
2019-03-17 00:51:04 +0900

    Fix: migrationファイル修正

--- db/migrate/20190316154234_remove_add_user_status_column.rb
+++
@@ -1,8 +0,0 @@
-class RemoveAddUserStatusColumn < ActiveRecord::Migration[5.2]
-  def change
-    if ENV['RAILS_ENV'] == 'production'
-      remove_column :users, :status, :integer
-      add_column :users, :status, 'integer USING CAST(status AS integer)', default: 1
-    end
-  end
-end
