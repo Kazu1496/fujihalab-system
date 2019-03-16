1a112e8b7e3ea5a3c10eee842ae926fe628bebd7
Kazumasa Ishikura <kazumasa1496@gmail.com>
2019-03-17 00:51:04 +0900

    Fix: migrationファイル修正

--- db/migrate/20190204150832_change_default_status_at_user.rb
+++ db/migrate/20190204150832_change_default_status_at_user.rb
@@ -1,5 +1,7 @@
 class ChangeDefaultStatusAtUser < ActiveRecord::Migration[5.2]
   def change
-    change_column :users, :status, :integer, default: 1
+    if ENV['RAILS_ENV'] == 'production'
+      change_column :users, :status, 'integer USING CAST(status AS integer)', default: 1
+    end
   end
 end
