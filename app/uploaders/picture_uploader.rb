601f266cc57d8838ab49849422031f82f32457e9
Kazumasa Ishikura <kazumasa1496@gmail.com>
2019-03-17 15:14:00 +0900

    Change: 画像周りをCloudinary経由させるようにする/その他細々修正

---
+++ app/uploaders/picture_uploader.rb
@@ -0,0 +1,11 @@
+class PictureUploader < CarrierWave::Uploader::Base
+  include Cloudinary::CarrierWave
+
+  version :retina do
+    process resize_to_fit: [640, 640]
+  end
+
+  version :schema do
+    process resize_to_fill: [800, 800, "Center"]
+  end
+end
---
+++ db/migrate/20190317051854_rename_user_image_column.rb
@@ -0,0 +1,5 @@
+class RenameUserImageColumn < ActiveRecord::Migration[5.2]
+  def change
+    rename_column :users, :image, :picture
+  end
+end
