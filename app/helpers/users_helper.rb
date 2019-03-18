b74addc0aa61b450f2008e3b6b427fc4c30ba486
Kazumasa Ishikura <kazumasa1496@gmail.com>
2019-03-18 21:37:31 +0900

    Change: ヘッダーに要素追加/view周りのリファクタ及び機能追加,修正

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,13 @@
 module UsersHelper
-  def user_image_tag(user, id_name)
+  def user_image_tag(user, id_name, width, height)
     if user.picture.present?
-      cl_image_tag(user.picture.retina.filename, width: 80, height: 80, id: id_name)
+      cl_image_tag(user.picture.retina.filename, width: width, height: height, id: id_name, alt: "#{user_name(user)}のプロフィール画像")
     else
-      image_tag("no_image.png", size: "80x80", id: id_name)
+      image_tag("no_image.png", widrh: width, height: height, id: id_name, alt: "#{user_name(user)}のプロフィール画像")
     end
+  end
+
+  def user_name(user)
+    name = user.nickname.present? ? user.nickname : user.name
   end
 end
