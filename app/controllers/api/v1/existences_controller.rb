0bd883f3931b274ab1abcd7ab8b2d99180317349
Kazumasa Ishikura <kazumasa1496@gmail.com>
2019-03-21 21:59:10 +0900

    Change: Bluetooth端末検知システムに合わせるために出退席処理を変える

--- app/controllers/api/v1/existences_controller.rb
+++ app/controllers/api/v1/existences_controller.rb
@@ -2,122 +2,58 @@ require 'securerandom'
 
 class Api::V1::ExistencesController < ApplicationController
   skip_before_action :require_sign_in!
+  before_action :white_list_ip?
 
   protect_from_forgery :except => [:post]
 
   def post
-    now_time = Time.now()
-    name = params[:name].downcase
-    password = params[:password]
-    status = params[:status]
-    user = User.find_by(name: name)
+    begin
+      now_time = Time.now()
+      status = params[:status]
+      user = User.find_by(address: params[:addr])
 
-    unless white_list(request.remote_ip)
-      render json: {status: 400, message: "許可されていない端末からのアクセスのため失敗しました。"} and return
-    end
+      if user.present?
+        if user.status == status
+          render json: {status: 400, message: "既に出席もしくは退席中のため失敗しました。"} and return
+        end
 
-    if user.blank?
-      user = User.new(
-        name: name,
-        password: password,
-        pixela_token: SecureRandom.hex(32)
-      )
+        username = user.nickname.present? ? user.nickname : user.name
+        if status
+          # Slack出席通知処理
+          notifier = Slack::Notifier.new(ENV['SLACK_WEBHOOK_URL'])
+          notifier.ping(
+            "[#{Rails.env}] #{username}さんが出席しました。"
+          )
 
-      # CreateUser
-      user_uri = URI.parse("https://pixe.la/v1/users/")
-      user_http = Net::HTTP.new(user_uri.host, user_uri.port)
+          existence = user.existences.order(:created_at).create(user_id: user.id)
+          user.update!(status: true)
+          existence.update!(enter_time: update_time(nil, now_time))
+        else
+          existence = user.existences.order(:created_at).last
+          existences = Existence.where(user_id: user.id)
+          total_time = total_time(existences)
 
-      user_http.use_ssl = true
-      user_http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+          #delete_pixel
+          delete_pixel(user, now_time.strftime("%Y%m%d"))
 
-      user_post_params = {
-        token: user.pixela_token,
-        username: user.name,
-        agreeTermsOfService: "yes",
-        notMinor: "yes"
-      }
-
-      user_req = Net::HTTP::Post.new(user_uri)
-      user_req.body = user_post_params.to_json
-
-      user_res = user_http.request(user_req)
-      user_result = JSON.parse(user_res.body)
-
-      # CreateGraph
-      graph_uri = URI.parse("https://pixe.la/v1/users/#{user.name}/graphs")
-      graph_http = Net::HTTP.new(graph_uri.host, graph_uri.port)
-
-      graph_http.use_ssl = true
-      graph_http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-      graph_post_params = {
-        id: "access-graph",
-        name: "#{user.name}-graph",
-        unit: "hour",
-        type: "int",
-        color: "shibafu"
-      }
-
-      graph_req = Net::HTTP::Post.new(graph_uri)
-      graph_req.body = graph_post_params.to_json
-      graph_req["X-USER-TOKEN"] = user.pixela_token
-
-      graph_res = graph_http.request(graph_req)
-      graph_result = JSON.parse(graph_res.body)
-
-      if user_result["isSuccess"] && graph_result["isSuccess"]
-        user.save
-        render json: {status: 200, message: "User create successfully!"}
+          if create_pixel(user, now_time, total_time) # CreatePixel
+            # Slack退席通知処理
+            notifier = Slack::Notifier.new(ENV['SLACK_WEBHOOK_URL'])
+            notifier.ping(
+              "[#{Rails.env}] #{username}さんが退席しました。"
+            )
+            user.update!(status: false, total_time: total_time)
+            existence.update!(exit_time: update_time(nil, now_time))
+            render json: {status: 200, message: "Pixel create successfully!"}
+          else
+            render json: {status: 400, message: "Pixel create faild..."}
+          end
+        end
       else
-        render json: {user_result: user_result, graph_result: graph_result} and return
+        render json: {status: 400, message: "ユーザー情報を取得できませんでした。"}
       end
-    end
-
-    if user.status == status
-      render json: {status: 400, message: "既に出席もしくは退席中のため失敗しました。"} and return
-    end
-
-    username = user.nickname.present? ? user.nickname : user.name
-    if status
-      # Slack出席通知処理
-      notifier = Slack::Notifier.new(ENV['SLACK_WEBHOOK_URL'])
-      notifier.ping(
-        "[#{Rails.env}] #{username}さんが出席しました。"
-      )
-
-      existence = user.existences.order(:created_at).create(user_id: user.id)
-      user.update!(status: true)
-      existence.update!(enter_time: update_time(nil, now_time))
-    else
-      existence = user.existences.order(:created_at).last
-      existences = Existence.where(user_id: user.id)
-      total_time = total_time(existences)
-
-      #delete_pixel
-      delete_pixel(user, now_time.strftime("%Y%m%d"))
-
-      if create_pixel(user, now_time, total_time) # CreatePixel
-        # Slack退席通知処理
-        notifier = Slack::Notifier.new(ENV['SLACK_WEBHOOK_URL'])
-        notifier.ping(
-          "[#{Rails.env}] #{username}さんが退席しました。"
-        )
-        user.update!(status: false, total_time: total_time)
-        existence.update!(exit_time: update_time(nil, now_time))
-        render json: {status: 200, message: "Pixel create successfully!"}
-      else
-        render json: {status: 400, message: "Pixel create faild..."}
-      end
+    rescue => e
+      render json: {status: 400, message: {class: e.class, error: e.message}}
     end
   end
-
-  private
-    def white_list(ip)
-      if ENV['RAILS_ENV'] == 'production'
-        return true if ENV["LAB_IP_ADDRESS"] == ip
-        false
-      else
-        true
-      end
-    end
 end
