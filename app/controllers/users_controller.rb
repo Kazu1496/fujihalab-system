358d752978dba47b18268bc1379d461cba8e57a5
Kazumasa Ishikura <kazumasa1496@gmail.com>
2019-03-21 21:59:10 +0900

    Change: Bluetooth端末検知システムに合わせるために出退席処理を変える

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
+  skip_before_action :require_sign_in!, only: [:new, :create]
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :already_sign_in?, only: [:new, :create]
+  before_action :white_list_ip?, only: [:new, :create]
 
   def index
     @users = User.where(status: true)
@@ -15,6 +18,78 @@ class UsersController < ApplicationController
 
   def member
     @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    now_time = Time.now()
+    username = @user.nickname.present? ? @user.nickname : @user.name
+
+    # CreateUser
+    user_uri = URI.parse("https://pixe.la/v1/users/")
+    user_http = Net::HTTP.new(user_uri.host, user_uri.port)
+
+    user_http.use_ssl = true
+    user_http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    user_post_params = {
+      token: @user.pixela_token,
+      username: @user.name,
+      agreeTermsOfService: "yes",
+      notMinor: "yes"
+    }
+
+    user_req = Net::HTTP::Post.new(user_uri)
+    user_req.body = user_post_params.to_json
+
+    user_res = user_http.request(user_req)
+    user_result = JSON.parse(user_res.body)
+
+    # CreateGraph
+    graph_uri = URI.parse("https://pixe.la/v1/users/#{@user.name}/graphs")
+    graph_http = Net::HTTP.new(graph_uri.host, graph_uri.port)
+
+    graph_http.use_ssl = true
+    graph_http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    graph_post_params = {
+      id: "access-graph",
+      name: "#{@user.name}-graph",
+      unit: "hour",
+      type: "int",
+      color: "shibafu"
+    }
+
+    graph_req = Net::HTTP::Post.new(graph_uri)
+    graph_req.body = graph_post_params.to_json
+    graph_req["X-USER-TOKEN"] = @user.pixela_token
+
+    graph_res = graph_http.request(graph_req)
+    graph_result = JSON.parse(graph_res.body)
+
+    puts user_result["isSuccess"]
+    puts graph_result["isSuccess"]
+
+    respond_to do |format|
+      if @user.save && user_result["isSuccess"] && graph_result["isSuccess"]
+        notifier = Slack::Notifier.new(ENV['SLACK_WEBHOOK_URL'])
+        notifier.ping(
+          "[#{Rails.env}] #{username}さんが出席しました。"
+        )
+
+        existence = @user.existences.create(user_id: @user.id, enter_time: update_time(nil, now_time))
+
+        sign_in(@user)
+        format.html { redirect_to @user, notice: 'ユーザー登録が完了しました。' }
+      else
+        flash[:alert] = "ユーザ登録ができませんでした。再度やり直してください"
+        format.html { render :new }
+      end
+    end
   end
 
   def update
@@ -34,6 +109,6 @@ class UsersController < ApplicationController
     end
 
     def user_params
-      params.require(:user).permit(:name, :nickname, :picture, :password, :password_confirmation)
+      params.require(:user).permit(:name, :nickname, :picture, :password, :password_confirmation, :address).merge(status: true, pixela_token: SecureRandom.hex(32))
     end
 end
