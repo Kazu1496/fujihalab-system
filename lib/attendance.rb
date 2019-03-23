b6f2652063c0bf5cc207b887170cf313d9e5277a
Kazumasa Ishikura <kazumasa1496@gmail.com>
2019-03-23 20:06:57 +0900

    Fix: 退席のときに総時間が保存されないバグ修正/スタイル追加

--- lib/attendance.rb
+++ lib/attendance.rb
@@ -6,13 +6,14 @@ module Attendance extend self
 
     users.each do |user|
       latest_existence = user.existences.last
+      latest_existence.update(exit_time: now_time - 60)
+      
       existences = Existence.where(user_id: user.id)
       total_time = controller.total_time(existences)
 
       controller.delete_pixel(user, now_time.strftime("%Y%m%d"))
       if controller.create_pixel(user, now_time, total_time)
         user.update!(status: false)
-        latest_existence.update(exit_time: now_time - 60)
       end
 
       user.update!(
