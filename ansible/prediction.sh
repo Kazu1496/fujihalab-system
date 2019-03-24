f9f86ddf9c4ae6633b7814f1dd61beee2951c324
2357gi <2357.gi@gmail.com>
2019-03-24 15:35:12 +0900

    [wip]

---
+++ ansible/prediction.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+HOSTS=inventory/hosts
+
+. ./functions.sh
+
+# pb 01-init-server.yml
+# pb 02-install_mysql.yml
+# pb 03-install_nodejs.yml
+# pb 04-setup_ruby.yml
+pb 05-gitconfig.yml
+
