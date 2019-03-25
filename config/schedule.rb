b4fff4597a02de2b2b088abe489e4dd9590fdfb0
Kazumasa Ishikura <kazumasa1496@gmail.com>
2019-03-25 14:54:13 +0900

    Fix: whenever周りのバグ修正

--- config/schedule.rb
+++ config/schedule.rb
@@ -7,6 +7,7 @@ require File.expand_path(File.dirname(__FILE__) + "/environment")
 rails_env = ENV['RAILS_ENV'] ||= 'development'
 set :environment, rails_env
 set :output, "#{Rails.root}/log/crontab.log"
+ENV.each { |k, v| env(k, v) }
 
 every 1.day, at: "11:59 pm" do
   rake "attendance:attendance"
